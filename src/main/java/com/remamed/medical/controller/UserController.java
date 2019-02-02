57ba2c7355596f695a67067bac605b04448d8a23
windfly <jifyzu@163.com>
2019-02-02 16:13:11 +0800

    git3

--- src/main/java/com/remamed/medical/controller/UserController.java
+++ src/main/java/com/remamed/medical/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     private UserService userService;
 
     /**
-     * @Description 用户注册信息的请求方法
+     * @Description 用户注册信息的请求方法ff
      * @Date 2019-01-31
      * @Param [JIfeng]
      * @return com.remamed.medical.util.RespResult
